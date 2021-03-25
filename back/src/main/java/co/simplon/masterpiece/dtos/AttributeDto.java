a5a417bf22186aba3dda5d9b7a90a89fdb1bdaf3
zd87 <daria.lecerf@gmail.com>
2021-03-25 12:09:41 +0100

    added import excel feature

--- back/src/main/java/co/simplon/masterpiece/dtos/AttributeDto.java
+++ back/src/main/java/co/simplon/masterpiece/dtos/AttributeDto.java
@@ -3,13 +3,17 @@ package co.simplon.masterpiece.dtos;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
+@ToString
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class AttributeDto {
 
 	@NotBlank
